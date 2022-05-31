97f87ebf50a58f01d8c35a1f6b8632ad09c5de56
Ysobella Torio <94037762+ysobella@users.noreply.github.com>
2022-05-31 22:43:22 +0800

    Add files via upload

--- searchByName.c
+++ searchByName.c
@@ -14,17 +14,18 @@ void searchByName(struct details entry[], int storedEntries)
     int i, j, k, l;
     char substring[21];
     int len; 
-    int ctr = 0;
+    int ctr = 0, entryCtr = 0, substringCtr = 0;
 
     printf("Search Name: ");
-    // getchar();    gets(substring);
-    scanf("%s", substring);
+    getchar();    gets(substring);
     len = strlen(substring);
     
     char arr[len + 1]; // array to access the substrings
+    int entries[storedEntries];
 
     for(i = 0; i < storedEntries; i++) // searching the array of strings
     {
+        substringCtr = 0;
         if(strlen(entry[i].name20) >= len)
         {
             for(j = 0; j < strlen(entry[i].name20) - len + 1; j++) // searching each character of a given string
@@ -41,10 +42,20 @@ void searchByName(struct details entry[], int storedEntries)
 
                 if(strcmp(substring, arr) == 0)
                 {
-                    printf("\nEntry Index: %d\nPokemon Name: %s\nPokemon Type: %s\nDescription: %s\n\n", (i + 1), entry[i].name20, entry[i].type20, entry[i].description50);
+                    if(substringCtr == 0)
+                    {
+                        printf("\nEntry Index: %d\nPokemon Name: %s\nPokemon Type: %s\nDescription: %s\n\n", (i + 1), entry[i].name20, entry[i].type20, entry[i].description50);
+                        entryCtr++;
+                        substringCtr++;
+                    }
                 }
             }
         }
+    }
+
+    if(entryCtr == 0)
+    {
+        printf("No entries found\n");
     }
 
     KeyContinue();
