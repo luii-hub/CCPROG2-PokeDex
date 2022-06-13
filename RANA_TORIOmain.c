b77601feac7051158c7f507fd380fcdbd4d82566
Ysobella Torio <94037762+ysobella@users.noreply.github.com>
2022-06-13 22:11:08 +0800

    Update RANA_TORIOmain.c

--- RANA_TORIOmain.c
+++ RANA_TORIOmain.c
@@ -14,7 +14,7 @@ TORIO, Ysobella, DLSU ID# 12172030
 #include <conio.h> 
 #include <windows.h> 
 #include <unistd.h> 
-#include "PokeDex.h"
+#include "RANA_TORIOheader.h"
 
 /* --- Main Function --- */
 int main()
